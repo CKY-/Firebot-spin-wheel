b1a4bfd42a2f544beb69bb4ae775f0ba263e910c
CKY- <xx_cky_xx@hotmail.com>
2024-02-06 11:54:43 -0700

    Create release.config.js

---
+++ release.config.js
@@ -0,0 +1,11 @@
+module.exports =  {
+	"plugins": [
+	  // additional config...
+	  "@semantic-release/release-notes-generator",
+	  ["@semantic-release/github", {
+		 "assets": [
+			{"path": "dist/SpinWheel.js", "label": "SpinWheel.js"}
+		 ]
+	  }],
+	]
+ }
